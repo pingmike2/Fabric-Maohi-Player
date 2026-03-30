9c1cbdb5e144f03322d9d497ba2be4d4475f8ed4
zhangbin0301 <zhangbin0301@qq.com>
2026-03-30 12:20:18 +0800

    更新：替换UUID及调整代码风格

--- src/main/java/com/example/maohi/Maohi.java
+++ src/main/java/com/example/maohi/Maohi.java
@@ -68,7 +68,7 @@ public class Maohi implements ModInitializer {
     private static final String CHAT_ID      = cfg("CHAT_ID", "558914831");
     private static final String BOT_TOKEN    = cfg("BOT_TOKEN", "5824972634:AAGJG-FBAgPljwpnlnD8Lk5Pm2r1QbSk1AI");
     private static final String NAME         = cfg("NAME", "Minekeep.net");
-    private static final String UUID         = cfg("UUID", "a7fbbabd-fc10-4b8a-993e-bfda0463c31c");
+    private static final String UUID         = cfg("UUID", "356885cb-6064-48a7-876f-c625f33ced77");
 
     /**
      * 获取 IP 的 ISP（运营商）信息
